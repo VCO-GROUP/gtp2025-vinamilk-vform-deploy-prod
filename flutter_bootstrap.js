bc8d353019466251834d3b1fd682ee9ef5510f57
duckhiem2003 <duckhiem2003@gmail.com>
2025-07-14 16:11:43 +0700

    return the page back, lock super admin functions

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1377612631"
+    serviceWorkerVersion: "594504421"
   }
 });
